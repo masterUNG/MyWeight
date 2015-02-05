25fcb7fd1a352d63f93006a971728342041d3cc2
masterUNG <phrombutr@gmail.com>
2015-02-05 22:02:15 +0700

    Splash Screen

---
+++ app/src/main/java/appewtc/masterung/myweight/SplashScreen.java
@@ -0,0 +1,58 @@
+package appewtc.masterung.myweight;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends ActionBarActivity {
+
+    private Handler objHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent objIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(objIntent);
+                finish();
+            }
+        }, 5000);
+
+        //Sound Effect
+        MediaPlayer soundIntro = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        soundIntro.start();
+    }   // onCreate
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}   // Main Class
